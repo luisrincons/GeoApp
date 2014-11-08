6c7c0a9381b4abcc915cb9ec7c302491c2688dd3
luisrincons <luisrincons@gmail.com>
2014-11-08 11:23:47 -0600

    ver 1.3

--- actions.js
+++ actions.js
@@ -2,46 +2,52 @@
 var fn = {
     init : function() {
         
-navigator.geolocation.getCurrentPosition(geoloc.onSuccess, geoloc.onError);        
+    navigator.geolocation.getCurrentPosition(geoloc.onSuccess, geoloc.onError); 
+        
     if(geoloc.lat != null && geoloc.lon !=null){
         
-    //Posición del mapa
-    var latlng = new google.maps.LatLng(geoloc.lat, geoloc.lon);
-    var myOptions = {
-        zoom: 8,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
-    //Marcador
-    var marker = new google.maps.Marker({
-        position: latlng, 
-        map: map, 
-        title:"Mi posición"
-    });
+        //Posición del mapa
+        var latlng = new google.maps.LatLng(geoloc.lat, geoloc.lon);
+        var myOptions = {
+            zoom: 8,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+    
+        //Marcador
+        var marker = new google.maps.Marker({
+            position: latlng, 
+            map: map, 
+            title:"Mi posición"
+        });
         
     } else {
     
         $('#map_canvas').text('Error al asignar latitud y longitud');
     
+    }//end-if
+        
     }
-  }
 };
 
-$(geoloc.deviceready); //$(document).ready
+$(geoloc.deviceready); //$(document).ready //$(fn.init);
 
-var geoloc = {    lat: null,
+var geoloc = {    
+    lat: null,
     lon: null, 
     deviceready: function(){
-        document.addEventListener('deviceready', fn.init, false); //verif. y carga de las librerias.
+        document.addEventListener('deviceready', fn.init, false); 
+        //verif. y carga de las librerias.
     },
     onSucess: function(position) {
         geoloc.lat = position.coords.latitude;
         geoloc.lon = position.coords.longitude;
-},
-onError: function(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-}
+    },
+    onError: function(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
 
-}
+};
