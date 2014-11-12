0efdd2635edda9b5415eabd897f46c995b028bb5
luisrincons <luisrincons@gmail.com>
2014-11-11 19:52:41 -0600

    v2.1

--- actions.js
+++ actions.js
@@ -1,13 +1,23 @@
 //actions.js
 var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
+    
+    //Posición del mapa
+    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var myOptions = {
+        zoom: 8,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+    //Marcador
+    var marker = new google.maps.Marker({
+        position: latlng, 
+        map: map, 
+        title:"Mi posición"
+    });
+    
 };
 
 // onError Callback receives a PositionError object
