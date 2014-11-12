ea40712a90de422a182bb643ec9ad6cfa994d913
luisrincons <luisrincons@gmail.com>
2014-11-11 20:12:10 -0600

    v2.3

--- actions.js
+++ actions.js
@@ -1,15 +1,6 @@
 //actions.js
 var onSuccess = function(position) {
 
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-    
     geoloc.lat = position.coords.latitude;
     geoloc.lon = position.coords.longitude;
     
