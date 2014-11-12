3b5005a3d1f4f967e803201a490a53476c6d588e
luisrincons <luisrincons@gmail.com>
2014-11-11 21:54:07 -0600

    v3.1

--- actions.js
+++ actions.js
@@ -8,16 +8,11 @@
 var fn = {
     init : function() {
         
-        alert("Iniciando...");
         navigator.geolocation.getCurrentPosition(geoloc.onSuccess, geoloc.onError); 
-        //alert('Latitude: '          + position.coords.latitude          + '\n' +
-        //  'Longitude: '         + position.coords.longitude         + '\n' +
-        //  'Altitude: '          + position.coords.altitude          + '\n' +
-        //  'Accuracy: '          + position.coords.accuracy          + '\n' +
-        //  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-        //  'Heading: '           + position.coords.heading           + '\n' +
-        //  'Speed: '             + position.coords.speed             + '\n' +
-        //  'Timestamp: '         + position.timestamp                + '\n');   
+        alert('Latitude: '      + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');   
 
     }
 };
