9e112b5c5297e3dacaf19f3e35820a85090b6065
luisrincons <luisrincons@gmail.com>
2014-11-11 19:38:25 -0600

    v2

--- actions.js
+++ actions.js
@@ -1,57 +1,20 @@
 //actions.js
-var fn = {
-    init : function() {
-    alert('Inicia...');   
-    navigator.geolocation.getCurrentPosition(geoloc.onSuccess, geoloc.onError); 
-    alert(geoloc.lat);
-    }
+var onSuccess = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
 };
 
-var geoloc = {    
-    device: function(){
-        document.addEventListener('deviceready', fn.init, false); 
-        //verif. y carga de las librerias.
-    },
-    onSucess: function(position) {
-        
-        geoloc.lat = position.coords.latitude;
-        geoloc.lon = position.coords.longitude;
-        
-        if(geoloc.lat != undefined && geoloc.lon != undefined){
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
 
-            //Posición del mapa
-            var latlng = new google.maps.LatLng(geoloc.lat, geoloc.lon);
-            var myOptions = {
-                zoom: 8,
-                center: latlng,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-
-            var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-
-            //Marcador
-            var marker = new google.maps.Marker({
-                position: latlng, 
-                map: map, 
-                title:"Mi posición"
-            });
-
-        } else {
-
-            $('#map_canvas').text('Error al asignar latitud y longitud');
-
-        }//end-if
-        
-             
-    },
-    
-    onError: function(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
-    }
-
-};
-
-//$(fn.init);
-
-$(geoloc.device); //$(document).ready //$(fn.init);
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
---
+++ actions_backup.js
@@ -0,0 +1,56 @@
+//actions.js
+var fn = {
+    init : function() {
+    alert('Inicia...');   
+    navigator.geolocation.getCurrentPosition(geoloc.onSuccess, geoloc.onError); 
+    }
+};
+
+var geoloc = {    
+    device: function(){
+        document.addEventListener('deviceready', fn.init, false); 
+        //verif. y carga de las librerias.
+    },
+    onSucess: function(position) {
+        
+        geoloc.lat = position.coords.latitude;
+        geoloc.lon = position.coords.longitude;
+        
+        if(geoloc.lat != undefined && geoloc.lon != undefined){
+
+            //Posición del mapa
+            var latlng = new google.maps.LatLng(geoloc.lat, geoloc.lon);
+            var myOptions = {
+                zoom: 8,
+                center: latlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+            //Marcador
+            var marker = new google.maps.Marker({
+                position: latlng, 
+                map: map, 
+                title:"Mi posición"
+            });
+
+        } else {
+
+            $('#map_canvas').text('Error al asignar latitud y longitud');
+
+        }//end-if
+        
+             
+    },
+    
+    onError: function(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+};
+
+//$(fn.init);
+
+$(geoloc.device); //$(document).ready //$(fn.init);
