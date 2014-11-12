eee87c5317e824db0fad75074f5d8adb3ae767e4
luisrincons <luisrincons@gmail.com>
2014-11-11 21:44:35 -0600

    v2.9

--- actions.js
+++ actions.js
@@ -1,67 +1,70 @@
 //actions.js
 
-function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-}
+//function onError(error) {
+//    alert('code: '    + error.code    + '\n' +
+//          'message: ' + error.message + '\n');
+//}
 
 var fn = {
     init : function() {
-     alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');   
-      //navigator.geolocation.getCurrentPosition(geoloc.onSuccess, geoloc.onError); 
+        
+        alert("Iniciando...");
+        //navigator.geolocation.getCurrentPosition(geoloc.onSuccess, geoloc.onError); 
+        //alert('Latitude: '          + position.coords.latitude          + '\n' +
+        //  'Longitude: '         + position.coords.longitude         + '\n' +
+        //  'Altitude: '          + position.coords.altitude          + '\n' +
+        //  'Accuracy: '          + position.coords.accuracy          + '\n' +
+        //  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+        //  'Heading: '           + position.coords.heading           + '\n' +
+        //  'Speed: '             + position.coords.speed             + '\n' +
+        //  'Timestamp: '         + position.timestamp                + '\n');   
+
     }
 };
 
 var geoloc = { 
     
-    device: function(){
-        document.addEventListener('deviceready', fn.init, false); 
-    },
+//    device: function(){
+//        document.addEventListener('deviceready', fn.init, false); 
+//    },
     
-    onSucess: function(position) {
+//    onSucess: function(position) {
                 
-        geoloc.lat = position.coords.latitude;
-        geoloc.lon = position.coords.longitude;
+//        geoloc.lat = position.coords.latitude;
+//        geoloc.lon = position.coords.longitude;
         
-        if(geoloc.lat != undefined && geoloc.lon != undefined){
+//        if(geoloc.lat != undefined && geoloc.lon != undefined){
 
             //Posición del mapa
-            var latlng = new google.maps.LatLng(geoloc.lat, geoloc.lon);
-            var myOptions = {
-                zoom: 8,
-                center: latlng,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
+//            var latlng = new google.maps.LatLng(geoloc.lat, geoloc.lon);
+//            var myOptions = {
+//                zoom: 8,
+//                center: latlng,
+//                mapTypeId: google.maps.MapTypeId.ROADMAP
+//            };
 
-            var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+//            var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
             //Marcador
-            var marker = new google.maps.Marker({
-                position: latlng, 
-                map: map, 
-                title:"Mi posición"
-            });
+//            var marker = new google.maps.Marker({
+//                position: latlng, 
+//                map: map, 
+//                title:"Mi posición"
+//            });
 
-        } else {
+//        } else {
 
-            $('#map_canvas').text('Error al asignar latitud y longitud');
+//            $('#map_canvas').text('Error al asignar latitud y longitud');
 
-        };//end-if
+//        };//end-if
         
              
-    },
+//    },
     
-    onError: function(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
-    }
+//    onError: function(error) {
+//        alert('code: '    + error.code    + '\n' +
+//              'message: ' + error.message + '\n');
+//    }
 
 };
 
