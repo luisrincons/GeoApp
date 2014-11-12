c152abfa325d10f878d6bfffcf686fc135b179db
luisrincons <luisrincons@gmail.com>
2014-11-11 23:18:41 -0600

    4.2

--- actions.js
+++ actions.js
@@ -1,5 +1,33 @@
 //actions.js
 var onSuccess = function(position) {
+        var geoloc.lat = position.coords.latitude;
+        var geoloc.lon = position.coords.longitude;
+        
+//        if(geoloc.lat != undefined && geoloc.lon != undefined){
+
+            //Posición del mapa
+            var latlng = new google.maps.LatLng(geoloc.lat, geoloc.lon);
+            var myOptions = {
+                zoom: 8,
+                center: latlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+            //Marcador
+            var marker = new google.maps.Marker({
+                position: latlng, 
+                map: map, 
+                title:"Mi posición"
+            });
+
+//        } else {
+
+//            $('#map_canvas').text('Error al asignar latitud y longitud');
+
+//        };//end-if  
+    
 };
 
 function onError(error) {
@@ -9,8 +37,8 @@ function onError(error) {
 
 var fn = {
     init : function() {
-        $('#version').text('1.6');
-        navigator.geolocation.getCurrentPosition(onSuccess, geoloc.onError); 
+        $('#version').text('1.7');
+        navigator.geolocation.getCurrentPosition(onSuccess, onError); 
         alert("Done!");
     }
 };
